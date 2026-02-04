195b0af40f0e650486028b88ab1011f69f0bb95f
Truong Quach Mai Tran <maitran@huongnghiepaau.com>
2026-02-04 11:11:22 +0700

    #Update init .NET Core API

---
+++ backend/TaskServerBE/TaskMS.Server/DTOs/AuthDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskMS.Server.DTOs
+{
+    public class AuthDto
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ backend/TaskServerBE/TaskMS.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskMS.Server.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+        public ICollection<TodoTask> TodoTasks { get; set; }
+
+    }
+}
