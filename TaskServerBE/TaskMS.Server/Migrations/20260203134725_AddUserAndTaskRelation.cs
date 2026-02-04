9e0cbc8408ce2e81e93c205d8bb33b5ea8fd8be9
Truong Quach Mai Tran <maitran@huongnghiepaau.com>
2026-02-04 09:40:48 +0700

    #Update

---
+++ TaskServerBE/TaskMS.Server/Migrations/20260203134725_AddUserAndTaskRelation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskMS.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndTaskRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "tasks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_UserId",
+                table: "tasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Username",
+                table: "users",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_users_UserId",
+                table: "tasks",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_users_UserId",
+                table: "tasks");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tasks_UserId",
+                table: "tasks");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "tasks");
+        }
+    }
+}
