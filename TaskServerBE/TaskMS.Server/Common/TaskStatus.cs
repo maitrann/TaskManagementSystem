b2eb09d1a55018a934d365b5e5df8873b29fbbb5
Truong Tran <91366164+maitrann@users.noreply.github.com>
2026-02-04 10:33:18 +0700

    Delete TaskServerBE directory

--- TaskServerBE/TaskMS.Server/Common/TaskStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskMS.Server.Common
-{
-    public enum TaskStatus
-    {
-        Todo = 0,
-        InProgress = 1,
-        Done = 2
-    }
-}
--- TaskServerBE/TaskMS.Server/DTOs/AuthDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskMS.Server.DTOs
-{
-    public class AuthDto
-    {
-        public string Username { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
